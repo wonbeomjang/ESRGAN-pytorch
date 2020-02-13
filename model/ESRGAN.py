9c4c6509be59d5ea3fa86b3b5c0e8b7c418dd5da
wonbeomjang <jtiger958@gmail.com>
2020-02-13 18:55:35 +0900

    change scale factor

--- model/ESRGAN.py
+++ model/ESRGAN.py
@@ -2,7 +2,7 @@ from model.block import *
 
 
 class ESRGAN(nn.Module):
-    def __init__(self, in_channels, out_channels, nf=64, gc=32, scale_factor=2, n_basic_block=23):
+    def __init__(self, in_channels, out_channels, nf=64, gc=32, scale_factor=4, n_basic_block=23):
         super(ESRGAN, self).__init__()
 
         self.conv1 = nn.Sequential(nn.ReflectionPad2d(1), nn.Conv2d(in_channels, nf, 3), nn.ReLU())
