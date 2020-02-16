f3a32e3b73f372a48bf3cbc8ff7652a4a4068feb
wonbeomjang <jtiger958@gmail.com>
2020-02-17 08:24:29 +0900

    update loss

--- loss/loss.py
+++ loss/loss.py
@@ -1,21 +1,18 @@
 import torch.nn as nn
-from torchvision.models.vgg import vgg16
+from torchvision.models.vgg import vgg19
 
 
 class PerceptualLoss(nn.Module):
     def __init__(self):
         super(PerceptualLoss, self).__init__()
 
-        vgg = vgg16(pretrained=True)
-        loss_network = nn.Sequential(*list(vgg.features)[:29]).eval()
+        vgg = vgg19(pretrained=True)
+        loss_network = nn.Sequential(*list(vgg.features)[:35]).eval()
         for param in loss_network.parameters():
             param.requires_grad = False
         self.loss_network = loss_network
         self.l1_loss = nn.L1Loss()
-        self.upsampler = nn.Upsample(size=224)
 
     def forward(self, high_resolution, fake_high_resolution):
-        high_resolution = self.upsampler(high_resolution)
-        fake_high_resolution = self.upsampler(fake_high_resolution)
         perception_loss = self.l1_loss(self.loss_network(high_resolution), self.loss_network(fake_high_resolution))
         return perception_loss
